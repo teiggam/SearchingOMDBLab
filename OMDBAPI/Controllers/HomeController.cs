181c927c88778368d1a2ff2ee72a6efc0bae6d74
Maggie Tamanini <82274835+teiggam@users.noreply.github.com>
2021-07-02 20:40:58 -0400

    Initial Commit

---
+++ OMDBAPI/Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using OMDBAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OMDBAPI.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult MovieSearch()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieSearch(string title)
+        {
+            MovieDAL md = new MovieDAL();
+            Movie m = md.GetMovie(title);
+            return View(m);
+        }
+
+        public IActionResult MovieNight()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult MovieNight(string title, string title2, string title3)
+        {
+            MovieDAL md = new MovieDAL();
+            List<Movie> movieNight = new List<Movie>();
+            Movie m = md.GetMovie(title);
+            movieNight.Add(m);
+            Movie m2 = md.GetMovie(title2);
+            movieNight.Add(m2);
+            Movie m3 = md.GetMovie(title3);
+            movieNight.Add(m3);
+
+            return View(movieNight);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OMDBAPI/Models/MovieDAL.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OMDBAPI.Models
+{
+    public class MovieDAL
+    {
+        public string CallAPI(string searchType, string title)
+        {
+            string key = "6f803922";
+            string url = @$"https://www.omdbapi.com/?{searchType}={title}&apikey={key}";
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader rd = new StreamReader(response.GetResponseStream());
+            string output = rd.ReadToEnd();
+            return output;
+        }
+        public Movie GetMovie(string title)
+        {
+            string json = CallAPI("t", title);
+            Movie m = JsonConvert.DeserializeObject<Movie>(json);
+            return m;
+
+        }
+    }
+}
